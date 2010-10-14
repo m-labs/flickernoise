722536c321d6d135f1d729c253ab38e3cd63d906
lekernel <sebastien.bourdeauducq@lekernel.net>
2010-10-14 16:10:29 +0200

    Support RTEMS shell

--- main.c
+++ main.c
@@ -17,22 +17,17 @@
 
 /* Inspired by "test.c" from Genode FX (DOpE-embedded demo application) */
 
-#ifdef RTEMS
 #include <bsp.h>
-#endif
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
 #include <dopelib.h>
 #include <vscreen.h>
-#ifndef RTEMS
-#include <SDL.h> /* for SDL_Quit */
-#else
 #include <bsp/milkymist_usbinput.h>
 #include <bsp/milkymist_ac97.h>
 #include <rtems/stackchk.h>
-#endif
+#include <rtems/shell.h>
 
 #include "cp.h"
 #include "audio.h"
@@ -43,23 +38,11 @@
 #include "flash.h"
 #include "filedialog.h"
 
-#ifdef RTEMS
 rtems_task gui_task(rtems_task_argument argument)
-#else
-int main(int argc, char *argv[])
-#endif
 {
 	printf("GUI task started\n");
 	if(dope_init())
-#ifdef RTEMS
 		return;
-#else
-		return 2;
-#endif
-#ifndef RTEMS
-	atexit(dope_deinit);
-	atexit(SDL_Quit); /* FIXME: this should be done by DoPE */
-#endif
 
 	init_cp();
         init_audio();
@@ -70,21 +53,41 @@ int main(int argc, char *argv[])
         init_shutdown();
 
 	dope_eventloop(0);
-#ifndef RTEMS
-	return 0;
-#endif
 }
 
-#ifdef RTEMS
 static rtems_id gui_task_id;
 rtems_task Init(rtems_task_argument argument)
 {
-	printf("Flickernoise starting...\n");
-	assert(rtems_task_create(rtems_build_name('G','U','I',' '), 1, 512*1024, RTEMS_PREEMPT | RTEMS_NO_TIMESLICE | RTEMS_NO_ASR | \
-	RTEMS_INTERRUPT_LEVEL(0), RTEMS_FLOATING_POINT, &gui_task_id) == RTEMS_SUCCESSFUL);
+	rtems_status_code sc;
+
+	printf("Starting GUI task...\n");
+
+	assert(rtems_task_create(rtems_build_name('G','U','I',' '), 100, 512*1024, RTEMS_PREEMPT | RTEMS_TIMESLICE | RTEMS_NO_ASR | \
+		RTEMS_INTERRUPT_LEVEL(0), RTEMS_FLOATING_POINT, &gui_task_id) == RTEMS_SUCCESSFUL);
 	assert(rtems_task_start(gui_task_id, gui_task, 0) == RTEMS_SUCCESSFUL);
+
+	/* disable shell because it uses 100% CPU (polled console driver) */
+#if 0
+	printf("Starting shell task...\n");
+	sc = rtems_shell_init(
+		"SHLL",
+		RTEMS_MINIMUM_STACK_SIZE * 4,
+		100,
+		"/dev/console",
+		false,
+		false,
+		NULL
+	);
+	if(sc != RTEMS_SUCCESSFUL)
+		printf("Unable to start shell (error code %d)\n", sc);
+#endif
+
 	rtems_task_delete(RTEMS_SELF);
 }
+
+#define CONFIGURE_SHELL_COMMANDS_INIT
+#define CONFIGURE_SHELL_COMMANDS_ALL
+#include <rtems/shellconfig.h>
 
 #define CONFIGURE_APPLICATION_NEEDS_CLOCK_DRIVER
 #define CONFIGURE_APPLICATION_NEEDS_CONSOLE_DRIVER
@@ -93,16 +96,21 @@ rtems_task Init(rtems_task_argument argument)
         AC97_DRIVER_TABLE_ENTRY, \
 	USBINPUT_DRIVER_TABLE_ENTRY
 
+#define CONFIGURE_APPLICATION_NEEDS_LIBBLOCK
+#define CONFIGURE_USE_IMFS_AS_BASE_FILESYSTEM
+
 #define CONFIGURE_EXECUTIVE_RAM_SIZE (16*1024*1024)
 
 #define CONFIGURE_LIBIO_MAXIMUM_FILE_DESCRIPTORS 16
-#define CONFIGURE_MAXIMUM_TASKS 2
-#define CONFIGURE_MAXIMUM_POSIX_THREADS 1
+#define CONFIGURE_MAXIMUM_TASKS 4
 #define CONFIGURE_MAXIMUM_POSIX_MUTEXES 8
+
+#define CONFIGURE_TICKS_PER_TIMESLICE 3
+#define CONFIGURE_MICROSECONDS_PER_TICK 10000
 
 #define CONFIGURE_RTEMS_INIT_TASKS_TABLE
 #define CONFIGURE_INIT_TASK_STACK_SIZE (8*1024)
-#define CONFIGURE_INIT_TASK_PRIORITY 120
+#define CONFIGURE_INIT_TASK_PRIORITY 100
 #define CONFIGURE_INIT_TASK_ATTRIBUTES 0
 #define CONFIGURE_INIT_TASK_INITIAL_MODES \
 	(RTEMS_PREEMPT | RTEMS_NO_TIMESLICE | RTEMS_NO_ASR | \
@@ -113,4 +121,3 @@ rtems_task Init(rtems_task_argument argument)
 
 #define CONFIGURE_INIT
 #include <rtems/confdefs.h>
-#endif
