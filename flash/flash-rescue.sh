6bb8c451e87e89232bdca26c78f86b30e4447573
Sebastien Bourdeauducq <sebastien@milkymist.org>
2011-03-08 18:45:33 +0100

    clean up flash.sh a little, remove some duplicate code (Xiangfu)

--- flash/flash-rescue.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-make -C ../src flickernoise.fbiz
-jtag -n flash-rescue.batch
--- flash/flash.sh
+++ flash/flash.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
+
 set -e
+
 make -C ../src flickernoise.fbiz
-jtag -n flash.batch
+
+BATCH_FILE=flash.batch
+
+ADDRESS=0x920000
+if [ "$1" == "rescue" ]; then
+    ADDRESS=0x2E0000
+fi
+
+batch() {
+    echo -e "$1" >> "${BATCH_FILE}"
+}
+
+rm -rf ${BATCH_FILE}
+
+batch "cable milkymist"
+batch "detect"
+batch "instruction CFG_OUT 000100 BYPASS"
+batch "instruction CFG_IN 000101 BYPASS"
+batch "pld load fjmem.bit"
+batch "initbus fjmem opcode=000010"
+batch "frequency 6000000"
+batch "detectflash 0"
+batch "endian big"
+batch "flashmem ${ADDRESS} ../src/flickernoise.fbiz noverify"
+
+jtag -n ${BATCH_FILE}
