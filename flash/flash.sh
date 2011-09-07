e8a29d2293127feba102bf27bd1738160ecdf1d8
Xiangfu Liu <xiangfu@sharism.cc>
2011-09-07 12:13:27 +0800

    flash.sh make parameters more flexible

--- flash/flash.sh
+++ flash/flash.sh
@@ -1,21 +1,22 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
 
 ADDRESS=0x920000
-if [ "$1" == "rescue" ]; then
+if [ "$1" == "rescue" ] || [ "$2" == "rescue" ]; then
     ADDRESS=0x2E0000
 fi
 
-TARGET=flickernoise.fbiz
-if [ "$2" == "nocompress" ]; then
-    TARGET=flickernoise.fbi
+TARGET=flickernoise.fbi
+if [ "$2" == "compress" ] || [ "$1" == "compress" ] ; then
+    TARGET=flickernoise.fbiz
 fi
 
 make -C ../src bin/${TARGET}
 
 BATCH_FILE=`mktemp`
 cat > ${BATCH_FILE}<<EOF
+
 cable milkymist
 detect
 instruction CFG_OUT 000100 BYPASS
@@ -27,7 +28,11 @@ detectflash 0
 endian big
 flashmem ${ADDRESS} ../src/bin/${TARGET} noverify
 pld reconfigure
+
 EOF
 
 jtag -n ${BATCH_FILE}
+more    ${BATCH_FILE}
 rm -f   ${BATCH_FILE}
+
+echo "Done"
