65038bd80c7d939251bd8e106a2cc8b487aa8e73
Sebastien Bourdeauducq <sebastien@milkymist.org>
2011-11-21 17:54:38 +0100

    pixbuf: basic support for JPEG

--- src/pixbuf/loaderjpeg.c
+++ src/pixbuf/loaderjpeg.c
@@ -16,12 +16,65 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <jpeglib.h>
 
 #include "pixbuf.h"
+#include "dither.h"
 #include "loaders.h"
 
 struct pixbuf *pixbuf_load_jpeg(char *filename)
 {
-	/* TODO */
-	return NULL;
+	struct pixbuf *ret;
+	FILE *fd;
+	struct jpeg_decompress_struct cinfo;
+	struct jpeg_error_mgr jerr;
+	unsigned char *pixels;
+	int i;
+	unsigned char **row_pointers;
+	
+	ret = NULL;
+	fd = fopen(filename, "rb");
+	if(fd == NULL) goto free0;
+	
+	cinfo.err = jpeg_std_error(&jerr); // TODO: get rid of the exit() code
+	jpeg_create_decompress(&cinfo);
+	jpeg_stdio_src(&cinfo, fd);
+	jpeg_read_header(&cinfo, TRUE);
+		
+	cinfo.out_color_space = JCS_RGB;
+	cinfo.out_color_components = 3;
+	cinfo.dither_mode = JDITHER_NONE;
+
+	pixels = malloc(3*cinfo.image_width*cinfo.image_height);
+	if(pixels == NULL) goto free2;
+	row_pointers = malloc(sizeof(char *)*cinfo.image_height);
+	if(row_pointers == NULL) goto free3;
+	for(i=0;i<cinfo.image_height;i++)
+		row_pointers[i] = &pixels[i*3*cinfo.image_width];
+
+	jpeg_start_decompress(&cinfo);
+	while(cinfo.output_scanline < cinfo.output_height)
+		jpeg_read_scanlines(&cinfo, &row_pointers[cinfo.output_scanline], 1);
+	ret = pixbuf_new(cinfo.image_width, cinfo.image_height);
+	if(ret == NULL) goto free5;
+	
+	if(!pixbuf_dither(ret->pixels, row_pointers, cinfo.image_width, cinfo.image_height)) {
+		pixbuf_dec_ref(ret);
+		ret = NULL;
+		goto free5;
+	}
+	
+free5:
+	jpeg_finish_decompress(&cinfo);
+free4:
+	free(row_pointers);
+free3:
+	free(pixels);
+free2:
+	jpeg_destroy_decompress(&cinfo);
+free1:
+	fclose(fd);
+free0:
+	return ret;
 }
